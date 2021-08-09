d310d8eb63f5b5b08961e6a5b349116666cca1dc
GamingLazer123 <chitnispinak@gmail.com>
2021-08-09 15:30:38 -0400

    last update to message monitor

--- messageMonitor.js
+++ messageMonitor.js
@@ -8,16 +8,16 @@ module.exports = {
                 
                 const Embed = new Discord.MessageEmbed()
                     .setColor(0xFF0000)
-                    .setTitle('Derogatory Phrase Monitor')
-                    .setDescription(user.toString() + ' has said the word "' + forbiddenWords[i] + '" in the phrase "' + msg + '" in the channel' + msg.channel.toString())
+                    .setTitle('Forbidden Phrase Monitor')
+                    .setDescription(user.toString() + ' has said the word "' + forbiddenWords[i] + '" in the channel ' + msg.channel.toString())
                     .setFooter("Created by the Tech Team")
                 client.channels.cache.find(channel => channel.id === "863215550519574558").send(Embed);
 
                 
                 const Embed2 = new Discord.MessageEmbed()
                     .setColor(0xFF0000)
-                    .setTitle('Derogatory Phrase Monitor')
-                    .setDescription('You have said a derogatory phrase. The organizers have been notified and may reach out to you.')
+                    .setTitle('Forbidden Phrase Monitor')
+                    .setDescription('You have said a forbidden phrase. The organizers have been notified and may reach out to you.')
                     .setFooter("Created by the Tech Team")
                     user.send(Embed2)
 
