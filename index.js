3e8239ab62802b198b352567a8bf961264c97c99
GamingLazer123 <chitnispinak@gmail.com>
2021-08-12 12:06:29 -0400

    cleaned up index.js, added an Embed message when you join server

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION" 
 global.client = client;
 global.Discord = Discord;
 client.login('ODQyMTAxOTUxODU1NzIyNTA3.YJwajg.JasBtu3t5zS6oO3cHn8zmJ-HMmA')
+const { addRole, setCompetitor } = require('./newMember')
 const commandHandler = require("./commands");
 const { monitor } = require('./messageMonitor');
 const firstMessage = require('./first-message')
@@ -14,30 +15,16 @@ client.on('ready', () => {
   //firstMessage.welcome()
 })
 
+client.on('guildMemberAdd', guildMember => {
+  addRole(guildMember)
+});
+
 client.on("message", commandHandler.handle);
 
 client.on("message", msg => {
   monitor(msg);
-});
-
-client.on("message", msg => {
-  if(msg.channel.id === '875210238050066452'){
-  if(msg.author.bot) return;
-  let addrole = msg.guild.roles.cache.find(role => role.id == "857826276751179776")
-  let removerole = msg.guild.roles.cache.find(role => role.id == "874839329237762109")
-  const person = msg.member
-    if(msg.content === 'I accept'){
-      person.roles.add(addrole);
-      person.roles.remove(removerole)
-      msg.delete()
-    }
-    else {
-      msg.delete()
-    }
+  if(msg.channel.id === '875210238050066452') {
+    setCompetitor(msg)
   }
 });
 
-client.on('guildMemberAdd', guildMember => {
-  let role = guildMember.guild.roles.cache.find(role => role.id === '874839329237762109');
-  guildMember.roles.add(role);
-});
---
+++ newMember.js
@@ -0,0 +1,40 @@
+module.exports = {
+    addRole: function(guildMember) {
+        let role = guildMember.guild.roles.cache.find(role => role.id === '874839329237762109');
+        guildMember.roles.add(role);
+    },
+
+    setCompetitor: function(msg) {
+        if(msg.author.bot) return;
+
+        let addrole = msg.guild.roles.cache.find(role => role.id == "857826276751179776")
+        let removerole = msg.guild.roles.cache.find(role => role.id == "874839329237762109")
+
+        const person = msg.member
+
+        if(msg.content === 'I accept'){
+            person.roles.add(addrole);
+            person.roles.remove(removerole)
+            const Embed = new Discord.MessageEmbed()
+                .setTitle('Welcome to the OHacksIO discord server!')
+                .setDescription('In case you haven\'t read the welcome message, please do so now in the <#858163151022850049> channel!' +
+                '\n\nWe look forward to having a great hackathon with you! Best of luck!')
+            const member = msg.guild.member(msg.author);
+
+            if (!member.dmChannel)
+                member.createDM().then(channel => {
+                channel.send(Embed).catch(() => console.log('Error sending a welcome DM!'))
+            })
+            
+            else {
+                member.dmChannel.send(Embed).catch(() => console.log('Error sending a welcome DM!'))
+            }
+            
+            msg.delete()
+        }
+
+        else {
+            msg.delete()
+        }
+    }
+}
