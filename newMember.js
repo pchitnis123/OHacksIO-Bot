3b3f8d9a5293a78f0f330411fb505b37400a5dd8
GamingLazer123 <chitnispinak@gmail.com>
2021-08-14 11:46:50 -0400

    made some changes about when users get messages

--- newMember.js
+++ newMember.js
@@ -2,6 +2,19 @@ module.exports = {
     addRole: function(guildMember) {
         let role = guildMember.guild.roles.cache.find(role => role.id === '874839329237762109');
         guildMember.roles.add(role);
+        const Embed = new Discord.MessageEmbed()
+                .setTitle('Welcome to the OHacksIO discord server!')
+                .setDescription('In case you haven\'t read the welcome message, please do so now in the <#858163151022850049> channel!' +
+                '\n\nIn order to join the rest of the server, please read the rules listed in the <#840055971492921355> channel and follow the directions given.' +
+                '\n\nWe look forward to having a great hackathon with you! Best of luck!')
+            if (!guildMember.dmChannel)
+                guildMember.createDM().then(channel => {
+                channel.send(Embed).catch(() => console.log('Error sending a welcome DM!'))
+            })
+            
+            else {
+                guildMember.dmChannel.send(Embed).catch(() => console.log('Error sending a welcome DM!'))
+            }
     },
 
     setCompetitor: function(msg) {
@@ -15,20 +28,6 @@ module.exports = {
         if(msg.content === 'I accept'){
             person.roles.add(addrole);
             person.roles.remove(removerole)
-            const Embed = new Discord.MessageEmbed()
-                .setTitle('Welcome to the OHacksIO discord server!')
-                .setDescription('In case you haven\'t read the welcome message, please do so now in the <#858163151022850049> channel!' +
-                '\n\nWe look forward to having a great hackathon with you! Best of luck!')
-            const member = msg.guild.member(msg.author);
-
-            if (!member.dmChannel)
-                member.createDM().then(channel => {
-                channel.send(Embed).catch(() => console.log('Error sending a welcome DM!'))
-            })
-            
-            else {
-                member.dmChannel.send(Embed).catch(() => console.log('Error sending a welcome DM!'))
-            }
             
             msg.delete().catch(error => {
                 if (error.code !== Discord.Constants.APIErrors.UNKNOWN_MESSAGE) {
